40355ba877e8c4f31a08187533bbf64638b6bccc
Jeremiah-Ng <72901442+Jeremiah-Ng@users.noreply.github.com>
2021-07-20 14:14:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,21 +17,24 @@ function setup() {
     canvas.center();
     canvas.position(450, 130)
     video = createCapture(VIDEO);
-
+    video.hide();
     posenet = ml5.poseNet(video, modelLoaded);
     posenet.on("pose", gotPoses);
 }
 remove1="";
 remove2="";
+right="";
+left="";
 function draw() {
     image(video, 0, 0, 600, 500);
 
     if (leftWristScore > 0.20) {
         fill("red");
         stroke("black");
-        circle(leftWristX, leftWristY, 20);q
+        circle(leftWristX, leftWristY, 20);
         number_leftWristY = Number(leftWristY);
         left = floor(number_leftWristY);
+        document.getElementById("speed").innerHTML="Song Playing - Dubwoofer Substep";
         if (left > right) {
             song2.pause();
             if(song1.isPlaying()){
@@ -46,6 +49,7 @@ function draw() {
         circle(rightWristX, rightWristY, 20);
         number_rightWristY = Number(rightWristY);
         right = floor(number_rightWristY);
+        document.getElementById("speed").innerHTML="Song Playing - Dubwoofer Substep";
         if (right > left) {
             song1.pause();
             if(song2.isPlaying()){
